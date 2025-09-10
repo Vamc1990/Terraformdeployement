3d5f968bd00ae6ae08fc35cd5ccda370cc657073
Vamc1990 <83383584+Vamc1990@users.noreply.github.com>
2025-09-10 23:30:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
   subscription_id = var.ARM_SUBSCRIPTION_ID
   client_id       = var.ARM_CLIENT_ID
   client_secret   = var.ARM_CLIENT_SECRET
-  tenant_id       = var.ARM_TENANT
+  tenant_id       = var.ARM_TENANT_ID
 }
 
 resource "azurerm_logic_app_workflow" "res-0" {
