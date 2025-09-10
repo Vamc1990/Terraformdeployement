4d5d3df0fa85c92c7dd5877c773c86b3ca2b1899
Vamc1990 <83383584+Vamc1990@users.noreply.github.com>
2025-09-10 23:41:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,6 @@ provider "azurerm" {
  # Use environment variables for credentials
   subscription_id = var.ARM_SUBSCRIPTION_ID
   client_id       = var.ARM_CLIENT_ID
-  client_secret   = var.ARM_CLIENT_SECRET
   tenant_id       = var.ARM_TENANT_ID
 }
 
